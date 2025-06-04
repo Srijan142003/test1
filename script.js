509a48c436051eb2320d22ae41bebf5caa163713
srijankundu14 <srijankundu14@gmail.com>
2025-06-04 06:20:35 +0000

    Add blurred and transparent effect to container

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Replace 'YOUR_UNSPLASH_ACCESS_KEY' with your actual Unsplash Access Key
 const unsplashAccessKey = "M5aPfdQpfSCeI4lRvSLL51n_ej5dJZjdNO2oGCC0F20"; 
 const keywords = 'technology, science, abstract'; // Keywords for image search
-const apiUrl = `https://api.unsplash.com/photos/random?query=${keywords}&client_id=${unsplashAccessKey}`;
+apiUrl = `https://api.unsplash.com/photos/random?query=${keywords}&client_id=${unsplashAccessKey}`;
 
 // Get the background container element
 const backgroundContainer = document.querySelector('.background-container');
